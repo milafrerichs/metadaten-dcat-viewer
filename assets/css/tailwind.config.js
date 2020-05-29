88c9681464b16cba2d1742d673c7884b4c431664
milafrerichs <mila.frerichs@gmail.com>
2020-05-29 11:35:38 +0200

    feat: add first city bonn
    add views for DCAT DKAN api

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  variants: {
+    borderWidth: ['responsive', 'first', 'hover', 'focus'],
+    backgroundColor: ['responsive', 'odd', 'hover', 'focus']
+  },
+  theme: {
+    extend: {
+      colors: {
+        civic: '#2b4c7e',
+        civic_accent: '#c7850c',
+      },
+      maxWidth: theme => {
+        return {
+          'screen-xl': theme('screens.xl'),
+          'testimonial': '100px',
+        }
+      },
+    }
+  },
+  plugins: [
+    require("tailwind-heropatterns")({
+      variants: [],
+      patterns: ["topography", "signal"],
+      colors: {
+          default: "#fff",
+          "blue-light": "#adc5ea",
+          "blue-lighter": "#abc7f1"
+        },
+      opacity: {
+          default: "0.4",
+          "20": "0.2",
+          "10": "0.1"
+        }
+    }),
+  ],
+};
